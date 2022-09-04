5b95e5ac1654b7f90152ccf482ee52d2109352c3
NeonOrbit <acc.neonic@gmail.com>
2022-09-04 21:37:35 +0000

    Initial setup

---
+++ app/src/main/java/app/neonorbit/mrvpatchmanager/App.kt
@@ -0,0 +1,14 @@
+package app.neonorbit.mrvpatchmanager
+
+import android.app.Application
+
+class App : Application() {
+    companion object {
+        lateinit var instance: App
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
