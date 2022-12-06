d878036eb4b5488fb6d73baddd006e608b22867c
NeonOrbit <acc.neonic@gmail.com>
2022-12-06 15:47:16 +0000

    Fix signature verification

--- app/src/main/java/app/neonorbit/mrvpatchmanager/apk/ApkUtil.kt
+++ app/src/main/java/app/neonorbit/mrvpatchmanager/apk/ApkUtil.kt
@@ -2,6 +2,9 @@ package app.neonorbit.mrvpatchmanager.apk
 
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.GET_META_DATA
+import android.content.pm.PackageManager.GET_SIGNATURES
+import android.content.pm.PackageManager.GET_SIGNING_CERTIFICATES
 import android.content.pm.Signature
 import android.graphics.drawable.Drawable
 import app.neonorbit.mrvpatchmanager.AppConfig
@@ -13,32 +16,33 @@ object ApkUtil {
     const val APK_MIME_TYPE = "application/vnd.android.package-archive"
 
     private fun PackageInfo.matchSignature(other: String): Boolean {
-        return this.signingInfo.apkContentsSigners.any { signature ->
+        return this.getSignatures().any { signature ->
             Signature(other) == signature
         }
     }
 
     private fun PackageInfo.matchSignature(other: PackageInfo): Boolean {
-        val otherSig = other.signingInfo.apkContentsSigners
-        return this.signingInfo.apkContentsSigners.any { it in otherSig }
+        return this.getSignatures().any { it in other.getSignatures() }
     }
 
     fun verifyFbSignature(file: File): Boolean {
-        return getSignatures(file)?.any { signature ->
+        return getSignatures(file).any { signature ->
             Signature(AppConfig.DEFAULT_FB_SIGNATURE) == signature
-        } == true
+        }
     }
 
     fun verifyMrvSignature(file: File): Boolean {
-        return getSignatures(file)?.any { signature ->
+        return getSignatures(file).any { signature ->
             Signature(AppConfig.MRV_PUBLIC_SIGNATURE) == signature
-        } == true
+        }
     }
 
     fun hasLatestMrvSignedApp(file: File): Boolean {
-        return getPackageInfo(file)?.let { apk ->
+        return getPackageInfo(file, true)?.let { apk ->
             getPackageInfo(apk.packageName, true)?.takeIf { installed ->
-                installed.matchSignature(AppConfig.MRV_PUBLIC_SIGNATURE)
+                try {
+                    installed.matchSignature(AppConfig.MRV_PUBLIC_SIGNATURE)
+                } catch (_: Exception) { false }
             }?.let { installed -> installed.longVersionCode >= apk.longVersionCode }
         } == true
     }
@@ -71,26 +75,28 @@ object ApkUtil {
                 Collections.singleton(apk.packageName)
             }).forEach { pkg ->
                 getPackageInfo(pkg, cert = true, meta = true)?.let { installed ->
-                    if (!installed.matchSignature(apk)) {
-                        conflicted[pkg] = installed.getAppName()
-                    }
+                    try {
+                        if (!installed.matchSignature(apk)) {
+                            conflicted[pkg] = installed.getAppName()
+                        }
+                    } catch (_: Exception) { }
                 }
             }
         }
         return conflicted
     }
 
-    private fun PackageInfo.getAppName(): String {
-        return AppServices.packageManager.getApplicationLabel(this.applicationInfo).toString()
+    private fun getSignatures(file: File): Array<out Signature> {
+        return getPackageInfo(file, true)?.getSignatures() ?: throw Exception(
+            "Failed to read apk signature"
+        )
     }
-
-    private fun getSignatures(file: File) = getPackageInfo(file, true)?.signingInfo?.apkContentsSigners
 
     @Suppress("deprecation")
     private fun getPackageInfo(pkg: String, cert: Boolean = false): PackageInfo? {
         return try {
             AppServices.packageManager.getPackageInfo(
-                pkg, if (cert) PackageManager.GET_SIGNING_CERTIFICATES else 0
+                pkg, if (cert) (GET_SIGNING_CERTIFICATES or GET_SIGNATURES) else 0
             )
         } catch (_: PackageManager.NameNotFoundException) { null }
     }
@@ -98,7 +104,7 @@ object ApkUtil {
     @Suppress("deprecation")
     private fun getPackageInfo(file: File, cert: Boolean = false): PackageInfo? {
         return AppServices.packageManager.getPackageArchiveInfo(
-            file.absolutePath, if (cert) PackageManager.GET_SIGNING_CERTIFICATES else 0
+            file.absolutePath, if (cert) (GET_SIGNING_CERTIFICATES or GET_SIGNATURES) else 0
         )
     }
 
@@ -116,12 +122,22 @@ object ApkUtil {
         )
     }
 
+    @Suppress("deprecation")
     private fun getPackageInfoFlags(cert: Boolean, meta: Boolean): Int {
         var flag = 0
-        if (meta) flag = flag or PackageManager.GET_META_DATA
-        if (cert) flag = flag or PackageManager.GET_SIGNING_CERTIFICATES
+        if (meta) flag = flag or GET_META_DATA
+        if (cert) flag = flag or GET_SIGNING_CERTIFICATES or GET_SIGNATURES
         return flag
     }
+
+    private fun PackageInfo.getAppName(): String {
+        return AppServices.packageManager.getApplicationLabel(this.applicationInfo).let {
+            if (it.startsWith(this.packageName)) this.packageName else it.toString()
+        }
+    }
+
+    @Suppress("deprecation")
+    private fun PackageInfo.getSignatures() = signingInfo?.apkContentsSigners ?: signatures
 
     data class ApkSimpleInfo(val pkg: String, val name: String, val version: String)
 }
