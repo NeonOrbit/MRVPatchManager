cc4526fedc81f78dc0843af0edb4c09812179f2a
NeonOrbit <acc.neonic@gmail.com>
2024-09-02 23:28:58 +0000

    Update apkmirror model

--- app/src/main/java/app/neonorbit/mrvpatchmanager/remote/data/ApkMirrorIFormData.kt
+++ app/src/main/java/app/neonorbit/mrvpatchmanager/remote/data/ApkMirrorIFormData.kt
@@ -5,16 +5,21 @@ import app.neonorbit.mrvpatchmanager.util.Utils
 import pl.droidsonroids.jspoon.annotation.Selector
 
 class ApkMirrorIFormData {
+    @Selector(value = "#download-link", attr = "href")
+    private var href: String? = null
+
     @Selector(value = "#filedownload", attr = "action")
-    private lateinit var action: String
+    private var action: String? = null
 
     @Selector(value = "#filedownload > input[name=id]", attr = "value")
-    private lateinit var id: String
+    private var id: String? = null
 
     @Selector(value = "#filedownload > input[name=key]", attr = "value")
-    private lateinit var key: String
+    private var key: String? = null
 
-    val link: String get() = Utils.absoluteUrl(ApkMirrorService.BASE_URL, "$action?id=$id&key=$key&forcebaseapk=true")
+    val link: String get() = Utils.absoluteUrl(ApkMirrorService.BASE_URL,
+        href ?: "$action?id=$id&key=$key&forcebaseapk=true"
+    )
 
     override fun toString(): String = "link: $link"
 }
